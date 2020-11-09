9ea92957f03b244a8777f8b40212406b37a2da87
Manuel Alejandro da Costa <manuelalejandrodacostacontreras>
2020-11-09 16:20:00 +0100

    chore: modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require ('html-webpack-plugin')
+const rootDir = path.resolve(__dirname, './src')
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, './dist/scripts'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'app-bundle.js',
     chunkFilename: '[name]-bundle.js'
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)?$/,
+        test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: 'ts-loader',
+        use: 'awesome-typescript-loader',
       },
       {
         test: /\.s?css$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.(jpe?g|png)$/,
+        test: /\.(jpe?g|png|gif|svg)$/,
         use: {
-          loader: 'url-loader?name=scripts/shared/images/[name].[ext]',
+          loader: 'url-loader',
           options: {
             limit: 1000,
-            fallback: 'file-loader?name=scripts/shared/images/[name].[ext]',
+            fallback: 'file-loader',
+            name: 'shared/[folder]/[name].[ext]',
             esModule: false,
           },
         },
@@ -34,9 +38,14 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(rootDir, 'index.html'),
+    }),
+    new webpack.ProgressPlugin(),
+],
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    publicPath: '/scripts/',
+    contentBase: path.resolve(rootDir),
     port: 3000,
     open: true,
   },
